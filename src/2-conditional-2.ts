a74080ace974a706b7f628f9507c5b01c3af70ba
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-07-31 11:29:42 +0100

    Add error text in comment

--- src/2-conditional-2.ts
+++ src/2-conditional-2.ts
@@ -1,5 +1,6 @@
 const toHumanFriendlyBoolean = <T extends boolean>(
   value: T
+  // Type '"Yes" | "No"' is not assignable to type 'T extends true ? "Yes" : "No"'.
 ): T extends true ? 'Yes' : 'No' => (value ? 'Yes' : 'No');
 
 toHumanFriendlyBoolean(true);
